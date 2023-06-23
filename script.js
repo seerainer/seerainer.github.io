4d59acf5ce1aa41bb30e3c05ee4cd534ff47d661
Philipp Seerainer <50533219+seerainer@users.noreply.github.com>
2023-06-24 00:42:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
-document.getElementsByTagName("details")[0].style.display = "block";
+const d = document.getElementsByTagName("details")[0];
 const m = window.atob("cGhpbGlwcEBzZWVyYWluZXIuY29t");
 const e = document.createElement("a");
 const p = document.getElementsByTagName("p")[0];
+d.style.display = "block";
 e.innerText = m;
 e.href = window.atob("bWFpbHRvOg==") + m;
 p.replaceChild(e, p.children[0]);
+d.onclick = () => {
+    const n = document.getElementsByTagName("pre")[0];
+
+    if (document.body.createTextRange) {
+        const r = document.body.createTextRange();
+        r.moveToElementText(n);
+        r.select();
+    } else if (window.getSelection) {
+        const selection = window.getSelection();
+        const r = document.createRange();
+        r.selectNodeContents(n);
+        selection.removeAllRanges();
+        selection.addRange(r);
+    }
+}
